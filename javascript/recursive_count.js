3d240459063fa62c91fff60a276953ad003e8f85
Keyvan Alonso <keyvanalonso@gmail.com>
2022-05-31 12:36:01 +0200

    completed lab

--- javascript/recursive_count.js
+++ javascript/recursive_count.js
@@ -1,6 +1,19 @@
 function recursiveCount(num = 0) {
-  // type your code here
+  if( num > 9){
+    return
+  }
+  console.log(num)
+  num++
+  recursiveCount(num)
 }
+
+// let count = 0;
+
+// while (count < 10) {
+//   console.log(count);
+//   ++count;
+// }
+
 
 if (require.main === module) {
   recursiveCount();
@@ -8,6 +21,3 @@ if (require.main === module) {
 
 module.exports = recursiveCount;
 
-// OPTIONAL
-// Please add your pseudocode to this file
-// And a written explanation of your solution
